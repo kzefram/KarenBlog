32aca724bf18e03253e81bc9925dbee3b2b9fdbc
kzefram <karenabourgeois@gmail.com>
2024-08-14 12:40:43 -0300

    created the header with menu, title, dark mode button, sign-in and hamburger menu

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,12 @@
-import { Navbar, TextInput } from 'flowbite-react';
-import { Link } from 'react-router-dom';
+import { Navbar, TextInput, Button } from 'flowbite-react';
+import { Link, useLocation } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
+import { FaMoon } from 'react-icons/fa';
 
 export default function Header() {
+  const path = useLocation().pathname;
   return (
-    <Navbar>
+    <Navbar className='bg:{gray-200}'>
       <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl flex font-semibold dark:white'>
         <span><img className='h-11' src="./src/assets/DesignNoBack.png" alt="Robot Logo" /></span>
         <span className='justify-center content-center'>Karen's Blog</span>
@@ -14,8 +16,34 @@ export default function Header() {
           placeholder='Search...'
           type='text'
           rightIcon={AiOutlineSearch }
+          className='hidden lg:inline'
         />
       </form>
+      <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+        <AiOutlineSearch />
+      </Button>
+      <div className='flex gap-2 md:order-2'>
+        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
+          <FaMoon />
+        </Button>
+        <Link to='/signin'>
+          <Button  pill>
+            Sign In
+          </Button>
+        </Link>
+        <Navbar.Toggle />
+      </div>
+      <Navbar.Collapse>
+        <Navbar.Link active={path === '/'} as={'div'}>
+          <Link to='/'>Home</Link>
+        </Navbar.Link >
+        <Navbar.Link active={path === '/about'} as={'div'}>
+          <Link to='/about'>About</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === '/projects'} as={'div'}>
+          <Link to='/projects'>Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
     </Navbar>
   )
 }
