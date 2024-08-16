3de7bcb32c84e61f583299328831a6a8e2e60d39
kzefram <karenabourgeois@gmail.com>
2024-08-16 17:38:33 -0300

    created middleware to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,19 +1,12 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs/dist/bcrypt.js";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
 
     if (!username || !email || !password || username === "" || email === "" || password === "") {
-        return res.status(400).json({ message: "All fields are required" });
-    }
-
-    if (password.length < 8) {
-        return res.status(400).json({ message: "Password must be at least 8 characters long" });
-    }
-
-    if (username.length < 5) {
-        return res.status(400).json({ message: "Username must be at least 5 characters long" });
+        next(errorHandler(400, "All input is required"));
     }
 
     const hashedPassword = bcryptjs.hashSync(password, 12);
@@ -29,7 +22,7 @@ export const signup = async (req, res) => {
     res.json({ message: "Signup successful" });
     }
     catch (error) {
-        res.status(500).json({ message: error.message });
+        next(error);
     }
 
 };
