83a2a0d1fa3fefc02bc88bb1e941733eb37a4e10
kzefram <karenabourgeois@gmail.com>
2024-08-19 17:11:11 -0300

    created the sign-up page with form

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,21 +1,32 @@
-import { Navbar, TextInput, Button } from 'flowbite-react';
-import { Link, useLocation } from 'react-router-dom';
-import { AiOutlineSearch } from 'react-icons/ai';
-import { FaMoon } from 'react-icons/fa';
+import { Navbar, TextInput, Button } from "flowbite-react";
+import { Link, useLocation } from "react-router-dom";
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon } from "react-icons/fa";
 
 export default function Header() {
   const path = useLocation().pathname;
   return (
     <Navbar className='bg:{gray-200}'>
-      <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl flex font-semibold dark:white'>
-        <span><img className='h-11' src="./src/assets/DesignNoBack.png" alt="Robot Logo" /></span>
-        <span className='justify-center content-center'>Karen's Blog</span>
+      <Link
+        to='/'
+        className='self-center whitespace-nowrap text-sm sm:text-xl flex font-semibold dark:white'
+      >
+        <span>
+          <img
+            className='h-11'
+            src='./src/assets/DesignNoBack.png'
+            alt='Robot Logo'
+          />
+        </span>
+        <span className='justify-center content-center text-white'>
+          Karen's Blog
+        </span>
       </Link>
       <form>
-        <TextInput 
+        <TextInput
           placeholder='Search...'
           type='text'
-          rightIcon={AiOutlineSearch }
+          rightIcon={AiOutlineSearch}
           className='hidden lg:inline'
         />
       </form>
@@ -27,23 +38,23 @@ export default function Header() {
           <FaMoon />
         </Button>
         <Link to='/signin'>
-          <Button  outline pill>
+          <Button outline pill>
             Sign In
           </Button>
         </Link>
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
-        <Navbar.Link active={path === '/'} as={'div'}>
+        <Navbar.Link active={path === "/"} as={"div"}>
           <Link to='/'>Home</Link>
-        </Navbar.Link >
-        <Navbar.Link active={path === '/about'} as={'div'}>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"} as={"div"}>
           <Link to='/about'>About</Link>
         </Navbar.Link>
-        <Navbar.Link active={path === '/projects'} as={'div'}>
+        <Navbar.Link active={path === "/projects"} as={"div"}>
           <Link to='/projects'>Projects</Link>
         </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
-  )
+  );
 }
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,66 @@
-
+import { Label } from "flowbite-react";
+import { Link } from "react-router-dom";
+import { TextInput } from "flowbite-react";
+import { Button } from "flowbite-react";
 
 export default function SignUp() {
   return (
-    <div>SignUp</div>
-  )
+    <div className='min-h-screen mt-20 dark:text-white'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+        {/* Left Side */}
+        <div className='flex-1'>
+          <Link to='/' className=' flex font-bold text-4xl'>
+            <span>
+              <img
+                className='h-12'
+                src='./src/assets/DesignNoBack.png'
+                alt='Robot Logo'
+              />
+            </span>
+            <span className='justify-center content-center text-gray-500'>
+              Karen's Blog
+            </span>
+          </Link>
+          <p className='text-sm mt-5 text-gray-500'>
+            This is a personal site for tech and environmental blogging. It
+            showcases my blog posts and abilities. Feel free to sign up and join
+            the community! You can signup with your email or with Google.
+          </p>
+        </div>
+        {/* Right Side */}
+        <div className='flex flex-1 text-gray-500'>
+          <form className='flex flex-col gap-4'>
+            <div className='text-gray-500'>
+              <Label
+                value='Your Username'
+                class='mb-2 text-sm text-gray-900 dark:text-white'
+              />
+              <TextInput type='text' placeholder='Username' id='username' />
+            </div>
+            <div className='text-gray-500'>
+              <Label value='Your Email' className='text-gray-500' />
+              <TextInput
+                type='email'
+                placeholder='name@domain.com'
+                id='email'
+              />
+            </div>
+            <div className='text-gray-500'>
+              <Label value='Your Password' className='text-gray-500' />
+              <TextInput type='password' placeholder='Password' id='password' />
+            </div>
+            <Button gradientDuoTone='purpleToBlue' type='submit'>
+              Sign Up
+            </Button>
+            <div className='flex gap-2 text-sm mt-5'>
+              <span>Have an account?</span>
+              <Link to='/signin' className='text-blue-500'>
+                Sign In
+              </Link>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
